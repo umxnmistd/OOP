2064ba3adcfd07b8a415e228940981b7bc53994b
Joel IV Sulit <joelivsulit@gmail.com>
2025-08-25 23:59:50 +0800

    Create Object.java

---
+++ Object.java
@@ -0,0 +1,13 @@
+package OOP;
+
+public class Object {
+    String name;
+    float age;
+
+    Object(String name, float age) {
+        this.name = name;
+        this.age = age;
+
+        System.out.println("You are " + name +" You are " + age + " Years Old.");
+    }
+}
